c40d8e41a44d2a927c1d42d4cabec6e26553a51c
M1K43L4 <cubon.mikaela@yahoo.com>
2019-12-08 22:15:39 -0600

    Add files via upload

--- Lab-2/keywordcountwave.py
+++ Lab-2/keywordcountwave.py
@@ -21,9 +21,8 @@ while fileisgood == False:
          print("is wav")
          fileisgood=True
     else:
-        print("is not wave. try again")
-        #exit() 
-
+        print("is not wav. try again")
+        
 
 speecht=sr.AudioFile(user_file)
 with speecht as source:
@@ -34,38 +33,78 @@ try:
 except Exception as e:
     print("Exception: "+str(e))
 
-#print("Enter Keyword: ")
 
-#keyword = "disgusting"
-#keyword = input()
+
+
+
+#function for all instances
+def find_all(a_str, sub):
+    start = 0
+    while True:
+        start = a_str.find(sub, start)
+        if start == -1: return
+        yield start
+        start += len(sub)
+
+#make list of all words
+wordlist = list()
+for word in s.split():
+    wordlist.append(word)
+
+#print(*wordlist)
+
 
 #append a newline character at the end
 s = s + "\n"
 
-keyword='is_string'
 endindex=s.find("\n")
-print(str(endindex))
-startindex = True
-count=0
+#print("end is at: " + str(endindex))
 
-for word in s.split():
 
-        print(word)
-        index = s.find(word)#returns start index
-        print(str(index))
-        if index!=0:
-            index-=1
-            character=s[index]
-            if character != " ":
-                startindex=False
-            
-            
-        
-            
-        
-#print("Keyword: " +keyword)
 
-#keycount = s.count(keyword)
+keywords= list()
+# for each word in word list, count the words
 
-#print("Keycount: " +str(keycount))
+for i in wordlist:#iterate through word list
+    l =list(find_all(s, i)) #list of indecies
+    #print(*l)
+    #print("word is: " + i)
+    count=0
+    index=0
+    for j in l:
+        #print(str(j))
+        #print(s[j])
+        if j == 0:
+            index= j + len(i)# a space when it is okay
+            character = s[index]
+            if character == ' ':
+                count+=1
+                #print("count is: " + str(count))
+        else:
+            index= j -1
+            character = s[index]
+            if character == ' ':
+                #count+=1
+                #print("count is: " + str(count))
+
+                index +=1#undo the minus so index=i
+                index+=len(i)
+                if index >= endindex:
+                    count+=1
+                    #print("count is: " + str(count))
+                else:
+                    character = s[index]
+                    if character == ' ': 
+                        count+=1
+                        #print("count is: " + str(count))
+
+    #print("final count is: " + str(count))
+    #if count >= 4, append to keywords list
+    if count >=4:
+        keywords.append(i)
+
+
+print("here are your keywords: ")
+keywords = list(dict.fromkeys(keywords))
+print(keywords)
 
